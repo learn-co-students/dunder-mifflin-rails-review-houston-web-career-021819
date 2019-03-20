25ed8e64b2870ee7a9f9be35947fbb55d364724b
moranja <jeffadammoran@gmail.com>
2019-03-20 14:27:53 -0500

    done

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,11 @@
 class Dog < ApplicationRecord
+  has_many :employees
+  
+  def self.sorted_dogs
+    Dog.all.sort_by{|dog| dog.employees.size}.reverse
+  end
+
+  def my_employees
+    Employee.all.select{|employee| employee.dog == self}
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,15 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :title, uniqueness: true
+  validate :alias_is_unique
+
+  def alias_is_unique
+    every_alias = Employee.all.map {|employee| employee.alias}
+    if self.alias == "none" || self.alias == ""
+    elsif every_alias.include?(self.alias)
+      errors.add(:alias, "Alias cannot be repeated.")
+    else # if it's not include, we're ok
+    end
+  end
 end
