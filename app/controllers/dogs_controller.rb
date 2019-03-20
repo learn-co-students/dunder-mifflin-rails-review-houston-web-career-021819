0a24382a7fc87b168550644d44e7b8e6023a2d32
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-03-20 17:27:25 -0500

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,28 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+    
+    def new
+        @employees = Employee.all
+        @dog = Dog.new
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        @employees = @dog.employees
+    end
+
+    def create
+        @dog = Dog.create(dog_params)
+        redirect_to dog_path(@dog)
+    end
+
+    
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
 end
