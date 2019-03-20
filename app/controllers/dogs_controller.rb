ef8d8ff6fe6e6a4d156c1a55dca591b7a23753f0
github username <github email address>
2019-03-20 15:07:32 -0500

    all done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,16 @@
 class DogsController < ApplicationController
+
+  def index
+    unsorted_dogs = Dog.all
+    @dogs = unsorted_dogs.sort_by { |dog| dog.employees.length }.reverse
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+
+
+
+
 end
