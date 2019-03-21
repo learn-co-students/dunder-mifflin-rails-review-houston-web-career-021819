d5f9fc11ce28081ff69ad0df4e2cd1520d4f76c1
Thomas_Hansen <ctrhansen@yahoo.com>
2019-03-21 11:26:52 -0500

    Done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,56 @@
 class EmployeesController < ApplicationController
+    #Create
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create 
+        @dogs = Dog.all
+        @employee = Employee.new(employee_params)
+        if @employee.valid?
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+
+    #Read
+    def index
+        @employees = Employee.all 
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+        @dog = Dog.find(@employee.dog_id)
+    end
+
+
+    #Update
+    def edit
+        @dogs = Dog.all
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @dogs = Dog.all
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        redirect_to employee_path(@employee)
+    end
+
+
+    #Delete
+
+    def destroy
+        @employee = Employee.find(params[:id])
+    end
+
+    #Strong Params
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :title, :alias, uniqueness: true
+
+    def to_s
+        self.first_name + " " + self.last_name
+    end
 end
